6e9d7da5a0d4b926eab1b2272464ed9d4ba67c41
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2021-12-16 21:50:45 -0300

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,32 @@
+//Promises
+//Unresolved - Resolved - Rejected
+
+// 3 States of Promises
+
+/* 
+waiting for something to finish.. - Unresolved - Pending
+somenthing finished and it all wen ok = Resolved
+
+something finished and something went bad - Rejected
+*/
+
+/* 
+Promise working out -- resolved (status) -- then (Callback)
+Promise something bad-- rejected (status) -- catch (Callback)
+*/
+
+const promise = new Promise((resolve, reject) => {
+	resolve() //or reject()
+})
+
+promise.then(()=>{
+	console.log('finally finished!')
+}).then(() => {
+	console.log('One more')
+}).then(() => {
+	console.log('More two')
+}).then(() => {
+	console.log('Third then')
+}).catch(() => {
+	console.log('Something wrong has happened')
+})
