4cc8f98382e8de8678bd3dcf3275987a1305ed21
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2021-12-18 16:26:31 -0300

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -18,3 +18,51 @@ class Model extends Car {
 }
 
 let myCar = new Model("Ford", "Mustang");
+
+/*
+
+*/
+
+class Aluno {
+
+  constructor(options){
+    this.name = options.name
+    this.age = options.age
+  }
+
+  somar(){
+    return this.num1 + 1
+  }
+
+  acao(){
+    return 'Uma nova ação'
+  }
+}
+
+class Grupo extends Aluno{
+    
+    constructor(nome){
+    	super(NaN, NaN)
+      this.name = nome
+      this.alunos = []
+    }
+
+    add(aluno){
+      this.alunos.push(aluno)
+    }
+
+    todos(){
+      return this.alunos
+    }
+ }
+
+ let aluno1 = new Aluno({name: 'Luiz', age: 89})
+ let aluno2 = new Aluno({name: 'Marcos', age: 15})
+
+ let group = new Grupo('novo grupo')
+ group.add(aluno1)
+ group.add(aluno2)
+
+console.log(group.name)
+console.log(group.todos())
+console.log(group.acao())
