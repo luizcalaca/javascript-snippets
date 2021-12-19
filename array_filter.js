afbf5e9c310cb72d593bb72a1fae084d1d64688c
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2021-12-19 11:55:30 -0300

    Update array_filter.js

--- array_filter.js
+++ array_filter.js
@@ -5,3 +5,13 @@ Use filter() when: you need to remove unwanted elements based on some condition(
 var uniqueArray = array.filter( function( elem, index, array ) {
     return array.indexOf( elem ) === index;
 } );
+
+/*
+
+*/
+
+array = [0,1,1,5,6,9,50,36]
+
+var uniqueArray = array.filter( function( elem, index, array ) {
+    return ( elem ) > 10;
+} );
